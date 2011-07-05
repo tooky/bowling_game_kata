247c9d518dbc574ce1cf1642a3e91cab6587fa0e
Steve Tooke <steve@heavi.es>
2011-07-05 23:11:29 +0100

    Add acceptance tests

---
+++ spec/game_spec.rb
@@ -0,0 +1,18 @@
+require 'game'
+
+#Examples: Twenty zeros produce zero.
+  #Twenty fours produce 80.
+  #Twelve tens produce 300
+describe Game do
+  context 'with twenty zeros' do
+    it 'produces 0'
+  end
+
+  context 'with twenty fours' do
+    it 'produces 80'
+  end
+
+  context 'with twelve tens' do
+    it 'produces 300'
+  end
+end
